6d0ebbee98b4d1c6ad6844c6ec37ba8064cbef51
Ryan Hefner <hi@ryanhefner.com>
2019-06-12 22:23:19 -0400

    Add test env settings to babel config

--- tools/babel-preset.js
+++ tools/babel-preset.js
@@ -14,6 +14,11 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = () => {
   return {
+    env: {
+      test: {
+        plugins: ['transform-object-rest-spread']
+      }
+    },
     presets: [
       ['@babel/preset-env', {
         'loose': true,
